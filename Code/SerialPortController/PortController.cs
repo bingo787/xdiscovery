1215028097be8408e398aded80475befb3feb3ef
bingo <787535780@qq.com>
2021-09-19 18:58:11 +0800

    USM锐化代码重构

--- Code/SerialPortController/PortController.cs
+++ Code/SerialPortController/PortController.cs
@@ -418,7 +418,7 @@ namespace SerialPortController
                 _serialPort.Write(command.ToArray(), 0, command.Count);
             }
             _isNeedFeeddingDog = false;
-#if DEBUG
+#if DEBUG000
             Console.WriteLine("Send-" + DateTime.Now.ToString("HH:mm:ss.ffff") + "=" + message);
 #endif
         }
